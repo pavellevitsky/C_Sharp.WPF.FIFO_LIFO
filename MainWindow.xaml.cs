a7687563b46afcdb58788f0afba0e52181e430ed
pavellev <pavellev@inter.net.il>
2020-11-12 21:38:39 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        Thread thread_RefreshBuffersContent;
+        Thread thread_SimulateBuffersTraffic;
+        Model.BufferFifo fifo_buffer;
+        Model.BufferLifo lifo_buffer;
+        bool stay_in_loop = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fifo_buffer = Model.Model.CreateFifoBuffer();
+            lifo_buffer = Model.Model.CreateLifoBuffer();
+            thread_RefreshBuffersContent = new Thread(new ThreadStart(RefreshBuffersContent));
+            thread_SimulateBuffersTraffic = new Thread(new ThreadStart(SimulateBuffersTraffic));
+            thread_RefreshBuffersContent.Start();
+            thread_SimulateBuffersTraffic.Start();
+        }
+
+        private void Button_fifo_write (object sender, RoutedEventArgs e)
+        {
+            if (fifo_write_data.Text != "")
+            {
+                byte data = byte.Parse(fifo_write_data.Text);
+                stay_in_loop = fifo_buffer.write(data);
+            }
+        }
+        private void Button_fifo_read (object sender, RoutedEventArgs e)
+        {
+            byte data = 0;
+            stay_in_loop = fifo_buffer.read(ref data);
+            fifo_read_data.Text = data.ToString();
+        }
+        private void Button_fifo_reset (object sender, RoutedEventArgs e)
+        {
+            fifo_buffer.reset();
+        }
+
+        private void Button_lifo_write (object sender, RoutedEventArgs e)
+        {
+            if (lifo_write_data.Text != "")
+            {
+                byte data = byte.Parse(lifo_write_data.Text);
+                stay_in_loop = lifo_buffer.write(data);
+            }
+        }
+        private void Button_lifo_read (object sender, RoutedEventArgs e)
+        {
+            byte data = 0;
+            stay_in_loop = lifo_buffer.read(ref data);
+            lifo_read_data.Text = data.ToString();
+        }
+        private void Button_lifo_reset (object sender, RoutedEventArgs e)
+        {
+            lifo_buffer.reset();
+        }
+
+        private void SimulateBuffersTraffic()
+        {
+            Random rnd = new Random();
+            byte data;
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+
+                Dispatcher.Invoke(() =>
+                {
+                    data = (byte)rnd.Next(0, 255);
+
+                    if ((data % 2) == 0)
+                    {
+                        if (fifo_buffer.write(data))
+                        {
+                            fifo_write_data.Text = data.ToString();
+                        }
+                        else
+                        {
+                            fifo_buffer.reset();
+                        }
+
+                        if (lifo_buffer.write(data))
+                        {
+                            lifo_write_data.Text = data.ToString();
+                        }
+                        else
+                        {
+                            lifo_buffer.reset();
+                        }
+                    }
+                    else
+                    {
+                        if (fifo_buffer.read(ref data))
+                        {
+                            fifo_read_data.Text = data.ToString();
+                        }
+                        else
+                        {
+                            fifo_buffer.reset();
+                        }
+
+                        if (lifo_buffer.read(ref data))
+                        {
+                            lifo_read_data.Text = data.ToString();
+                        }
+                        else
+                        {
+                            lifo_buffer.reset();
+                        }
+                    }
+                });
+            }
+        }
+
+        private void RefreshBuffersContent()
+        {
+            while (stay_in_loop)
+            {
+                Thread.Sleep(1000);
+
+                Dispatcher.Invoke(() =>
+                {
+                    fifo_size.Text = fifo_buffer.get_size().ToString();
+                    fifo_count.Text = fifo_buffer.get_count().ToString();
+                    fifo_head.Text = fifo_buffer.get_head().ToString();
+                    fifo_tail.Text = fifo_buffer.get_tail().ToString();
+                    fifo_data.Text = fifo_buffer.get_buffer_string().ToString();
+
+                    lifo_size.Text = lifo_buffer.get_size().ToString();
+                    lifo_count.Text = lifo_head.Text = lifo_buffer.get_count().ToString();
+                    lifo_data.Text = lifo_buffer.get_buffer_string().ToString();
+                });
+            }
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,173 @@
+﻿namespace Model
+{
+    public class Model
+    {
+        public static BufferFifo CreateFifoBuffer()
+        {
+            return new BufferFifo();
+        }
+        public static BufferLifo CreateLifoBuffer()
+        {
+            return new BufferLifo();
+        }
+    }
+
+    public class BufferFifo
+    {
+        private const int BUFFER_SIZE = 20;
+        private byte[] BUFFER_DATA = new byte[BUFFER_SIZE] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int count = 0;
+        private int head = 0;
+        private int tail = 0;
+
+        public bool write(byte data)
+        {
+            bool exit_code = false;
+
+            if (count != BUFFER_SIZE)
+            {
+                head = (head + 1) % BUFFER_SIZE;
+                BUFFER_DATA[head] = data;
+                count++;
+                exit_code = true;
+            }
+
+            return exit_code;
+        }
+        public bool read(ref byte data)
+        {
+            bool exit_code = false;
+
+            if (count != 0)
+            {
+                count--;
+                tail = (tail + 1) % BUFFER_SIZE;
+                data = BUFFER_DATA[tail];
+                exit_code = true;
+            }
+
+            return exit_code;
+        }
+        public void reset()
+        {
+            count = 0;
+            head = 0;
+            tail = 0;
+        }
+        public int get_size()
+        {
+            return BUFFER_SIZE;
+        }
+        public int get_count()
+        {
+            return count;
+        }
+        public int get_head()
+        {
+            return head;
+        }
+        public int get_tail()
+        {
+            return tail;
+        }
+        public string get_buffer_string()
+        {
+            string buffer_string = "";
+
+            for (int i = 0; i < BUFFER_SIZE; i++)
+            {
+                if (count == 0)
+                {
+                    buffer_string += "* ";
+                }
+                else if (tail < head)
+                {
+                    if ((i > tail) && (i <= head))
+                    {
+                        buffer_string = buffer_string + BUFFER_DATA[i].ToString() + " ";
+                    }
+                    else
+                    {
+                        buffer_string += "* ";
+                    }
+                }
+                else
+                {
+                    if ((i <= head) || (i > tail))
+                    {
+                        buffer_string = buffer_string + BUFFER_DATA[i].ToString() + " ";
+                    }
+                    else
+                    {
+                        buffer_string += "* ";
+                    }
+                }
+            }
+
+            return buffer_string;
+        }
+    }
+
+    public class BufferLifo
+    {
+        private const int BUFFER_SIZE = 30;
+        private byte[] BUFFER_DATA = new byte[BUFFER_SIZE] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int count = 0;
+
+        public bool write (byte data)
+        {
+            bool exit_code = false;
+
+            if (count != BUFFER_SIZE)
+            {
+                BUFFER_DATA[count] = data;
+                count++;
+                exit_code = true;
+            }
+
+            return exit_code;
+        }
+        public bool read (ref byte data)
+        {
+            bool exit_code = false;
+
+            if (count != 0)
+            {
+                count--;
+                data = BUFFER_DATA[count];
+                exit_code = true;
+            }
+
+            return exit_code;
+        }
+        public void reset()
+        {
+            count = 0;
+        }
+        public int get_size()
+        {
+            return BUFFER_SIZE;
+        }
+        public int get_count()
+        {
+            return count;
+        }
+        public string get_buffer_string()
+        {
+            string buffer_string = "";
+
+            for (int i = 0; i < BUFFER_SIZE; i++)
+            {
+                if (i < count)
+                {
+                    buffer_string = buffer_string + BUFFER_DATA[i].ToString() + " ";
+                }
+                else
+                {
+                    buffer_string += "* ";
+                }
+            }
+            return buffer_string;
+        }
+    }
+}
